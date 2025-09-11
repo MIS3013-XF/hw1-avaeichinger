992a5d50003eae760cdd5eaa9b02bb424e4dcf89
Ava Eichinger <Ava.C.Eichinger-1@ou.edu>
2025-09-11 18:14:01 -0500

    Console Added.

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,11 +1,12 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Ava Eichinger
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,47 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            string itemname;
+            double subtotal;
+            double salestax;
+            double salestotal;
+
+            Console.WriteLine("What is the item you're purchasing?");
+            itemname = Console.ReadLine();
+
+            Console.WriteLine("How many tickets do you want to buy?");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("What is the price of each ticket?");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            const double tax_rate = 0.085;
+            
+             subtotal = (price * quantity);
+             salestax = (tax_rate *  subtotal);
+             salestotal = (subtotal + salestax);
+
+         
+
+            Console.WriteLine($"Your Subtoal is " + subtotal.ToString("C2"));
+            Console.WriteLine($"Your Salestax is "  + salestax.ToString("C2"));
+            Console.WriteLine($"Your Salestotal is " + salestotal.ToString("C2"));
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+
+
+
+            
+
+
+            
+        
+            
+
+           
         }
     }
 }
